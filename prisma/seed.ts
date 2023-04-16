db0538eb575284c3d54bc68430cd50744d776504
Lucas Torres <lucastorresfellicio@gmail.com>
2023-04-15 21:10:03 -0300

    feat: add seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  await prisma.usuario.upsert({
+    where: { email: 'lucatsf@icloud.com' },
+    update: {},
+    create: {
+      id: 1,
+      nome: "Lucas",
+      senha: "Luc@s123",
+      email: "lucatsf@icloud.com",
+      perfil: "ROOT"
+    },
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
