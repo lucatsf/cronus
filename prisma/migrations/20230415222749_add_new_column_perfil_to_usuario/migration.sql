a6b4eb8a5d2b5d4bb19f760b2b8f4482fbcd0fe8
Lucas Torres <lucastorresfellicio@gmail.com>
2023-04-15 21:11:38 -0300

    feat: remove migrations

--- prisma/migrations/20230415222749_add_new_column_perfil_to_usuario/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "EnumPerfil" AS ENUM ('ADMIN', 'USUARIO', 'ROOT');
-
--- AlterTable
-ALTER TABLE "usuarios" ADD COLUMN     "perfil" "EnumPerfil" NOT NULL DEFAULT 'USUARIO';
